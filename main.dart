5d1a07daa3b7a201b4ffbdd7514a475386250cb6
jane <janemuthoka@gmail.com>
2024-04-11 06:23:09 -0700

    portfolio practice

---
+++ main.dart
@@ -0,0 +1,17 @@
+
+void main() {
+// declaring variables
+String name = "John"; //must be in quotes
+String address = "Kenya";  //must be in quotes
+num age = 20; //whole numbers
+num height = 5.9;//decimal numbers
+bool isMarried = false; //boolean
+   
+// printing variables value   
+print("Name is $name");
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married Status is $isMarried");
+}
+
